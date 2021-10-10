6c366ec89f372e398b2fbc4c239f0774c96c0776
Guy Mazinter <gmazinter@gmail.com>
2021-10-10 19:30:40 +0300

    initial commit

---
+++ components/primitives/Card/Card.tsx
@@ -0,0 +1,5 @@
+import tw from "twin.macro";
+
+const Card = tw.div`border-8 m-4 p-10 bg-red-400`;
+
+export default Card;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import GlobalStyles from "../components/GlobalStyles";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div>
+			<GlobalStyles />
+			<Component {...pageProps} />
+		</div>
+	);
 }
-export default MyApp
+export default MyApp;
